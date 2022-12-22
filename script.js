d1a8b83152954a5f0017cf109e2c73dd628aa1fa
Michael <55346444+hellawi@users.noreply.github.com>
2022-12-22 13:22:55 -0800

    added dom homework

---
+++ script.js
@@ -0,0 +1,19 @@
+function fontfamily() {
+    let element = document.getElementById("MegaPuperBlock1");
+    element.style.fontFamily = "serif";
+}
+function colorText() {
+    let elem = document.getElementById("MegaPuperBlock2");
+    elem.style.color = "blue";
+}
+function borderColor() {
+    document.getElementById("MegaPuperBlock3").style.borderColor = "yellow";
+}
+function backgroundColor() {
+    let body = document.getElementById("body");
+    body.style.backgroundColor = "rgb(63,94,251)";
+}
+function borderRadius() {
+    let border = document.getElementById("MegaPuperBlock5");
+    border.style.borderRadius = '5px';
+}
