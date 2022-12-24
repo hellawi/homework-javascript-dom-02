16c44274000a12b6c93a2d7bc086d911e473885f
Michael <55346444+hellawi@users.noreply.github.com>
2022-12-23 20:14:25 -0800

    DOM 2-nd commit

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ function fontfamily() {
 }
 function colorText() {
     let elem = document.getElementById("MegaPuperBlock2");
-    elem.style.color = "blue";
+    elem.style.color = `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
 }
 function borderColor() {
-    document.getElementById("MegaPuperBlock3").style.borderColor = "yellow";
+    document.getElementById("MegaPuperBlock3").style.borderColor = `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
 }
 function backgroundColor() {
     let body = document.getElementById("body");
-    body.style.backgroundColor = "rgb(63,94,251)";
+    body.style.backgroundColor = `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
 }
 function borderRadius() {
     let border = document.getElementById("MegaPuperBlock5");
     border.style.borderRadius = '5px';
 }
+
+
+function getRandom(){
+    return Math.floor(Math.random() * (255 - 0) + 0);
+} 
